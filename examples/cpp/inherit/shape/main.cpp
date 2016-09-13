c32675fded1a55b50b91ee041391a5a08b1ed5dc
Brian Malloy <malloy@clemson.edu>
2016-09-13 11:43:59 -0400

    new changes

---
+++ examples/cpp/inherit/shape/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Shape {
+public:
+  Shape(const std::string& n) : name(n) {}
+  const std::string& getName() const { return name; }
+  virtual float area() const = 0;
+private:
+  std::string name;
+};
+
+class Circle : public Shape {
+public:
+  Circle(const std::string& n, float r) : Shape(n), radius(r) {}
+  virtual float area() const { return 3.14*radius*radius; } 
+private:
+  float radius;
+};
+
+void printArea(const Shape* s) {
+  std::cout << "Area of " << s->getName() << " is " << s->area(); 
+  std::cout << std::endl;
+}
+
+int main() {
+  printArea(new Circle("circle", 5.0));
+}
